59e8a1c8495a27d825d3713e7ba1d567a398579c
Alexandr <AlMi@Mac-mini-Alexandr.local>
2018-04-27 12:04:10 +0300

    add header and hero

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -1,10 +1,34 @@
-
+import Vibrant from 'node-vibrant'
 
 window.addEventListener('load', () => {
-  let $btnNav = $('.header__btn-nav'),
-      $navContainer    = $('.header__nav-container')
+  let $btnNav         = $('.header__btn-nav'),
+      $navContainer   = $('.header__nav'),
+      $content        = $('.content'),
+      $heroBg         = $('.hero__bg-img'),
+      bookImgSrc      = document.querySelector('.hero__book-img').src,
+      dominantColors;
+
+  let v = new Vibrant(bookImgSrc);
+  v.getPalette((err, palette) => {
+    console.log( palette );
+    dominantColors = [...palette.Vibrant._rgb];
+    $heroBg.css("fill", `rgba(${dominantColors}, 1.00)`);
+  });
+
+  function toggleNav() {
+    $($navContainer).toggleClass('show');
+    $($content).toggleClass('blur');
+  }
+
+  $($content).on('click', (e) => {
+    if(e.currentTarget.classList.contains('blur')) {
+      e.preventDefault();
+      toggleNav();
+    }
+  });
+
 
   $($btnNav).on('click', () => {
-    $($navContainer).toggleClass('show');
+    toggleNav();
   })
 });
